9b46453f675feafc238b4167ad9bd63236943849
Greg Malatino <gregory.malatino@tenlegs.com>
2016-08-15 23:13:25 -0400

    Readded application and home routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+	this.route('application', function() {
+		this.route('home', {
+			'path': '/'
+		});
+	});
 });
 
 export default Router;
