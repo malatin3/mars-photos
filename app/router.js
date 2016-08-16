c1f6d2b13c22e887893a50f01434bbd0202069f5
Greg Malatino <gregory.malatino@tenlegs.com>
2016-08-15 23:09:35 -0400

    Rebuilt ember app with higher version

--- app/router.js
+++ app/router.js
@@ -2,15 +2,11 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-	location: config.locationType
+  location: config.locationType,
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
-	this.route('application', function() {
-		this.route('home', {
-		path: '/'
-	});
-	});
 });
 
 export default Router;
